a7b669889f9521b2cd1204c1d6057c4d10c55001
H22JUNG <h2jung6875@naver.com>
2024-07-11 21:45:02 +0900

    init

---
+++ mmca_api/src/main/java/kaw/levelup/mmca_api/MmcaApiApplication.java
@@ -0,0 +1,13 @@
+package kaw.levelup.mmca_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmcaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmcaApiApplication.class, args);
+	}
+
+}
---
+++ mmca_api/src/test/java/kaw/levelup/mmca_api/MmcaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kaw.levelup.mmca_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmcaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
