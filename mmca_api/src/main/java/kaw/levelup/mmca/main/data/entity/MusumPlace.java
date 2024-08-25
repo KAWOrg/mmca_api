72e9af7345285feead387d8c45cebb6e1b204915
wooheesu <hsoo99321@gmail.com>
2024-08-25 23:14:00 +0900

    Revert "어노테이션, 주석 정리"
    This reverts commit c0674c95d5af821fc4d82494283e9f1febd99812.

--- mmca_api/src/main/java/kaw/levelup/mmca/main/data/entity/MusumPlace.java
+++ mmca_api/src/main/java/kaw/levelup/mmca/main/data/entity/MusumPlace.java
@@ -18,7 +18,9 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @Table(name = "musum_place")
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED) 
+//@Builder
 public class MusumPlace {
 	
 	@Id
@@ -45,6 +47,8 @@ public class MusumPlace {
 	@Comment("미술관 코드")
 	@Column(length = 3, nullable = true)
 	String musumCd;
+	
+	
 	
 	@Builder
 	public MusumPlace(Long placeId, String placeTypeCd, String placeNm, String placeDscrp, String musumCd) {
--- mmca_api/src/main/java/kaw/levelup/mmca/main/data/vo/MusumInfoDTO.java
+++ mmca_api/src/main/java/kaw/levelup/mmca/main/data/vo/MusumInfoDTO.java
@@ -1,9 +1,11 @@
 package kaw.levelup.mmca.main.data.vo;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 @Builder
 public class MusumInfoDTO {
 	
